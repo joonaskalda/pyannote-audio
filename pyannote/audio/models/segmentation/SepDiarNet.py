fix order of average pool

pyannote/audio/models/segmentation/SepDiarNet.py
@@ -201,8 +201,9 @@ class SepDiarNet(Model):
         decoded_sources = decoded_sources.transpose(1, 2)
         outputs = torch.flatten(masked_tf_rep, start_dim=0, end_dim=1)
         # shape (batch * nsrc, nfilters, nframes)
-        outputs = self.average_pool(outputs)
         outputs = torch.cat((outputs, tf_rep.repeat(3,1,1)), dim=1)
+        outputs = self.average_pool(outputs)
+        #outputs = torch.cat((outputs, tf_rep.repeat(3,1,1)), dim=1)
         outputs = outputs.transpose(1, 2)
         # shape (batch, nframes, nfilters)
         if self.use_lstm: